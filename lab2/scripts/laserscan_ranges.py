60eb5c91f1f621a8aeb50896efc7ee5b228b77fa
Connor Guzikowski <connorguzi@gmail.com>
2021-10-16 01:40:36 +0000

    Completed lab2

--- lab2/scripts/laserscan_ranges.py
+++ lab2/scripts/laserscan_ranges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#from yaml import scan
 import rospy
 import numpy as np
 from std_msgs.msg import String, Float32
@@ -8,13 +9,22 @@ from lab2.msg import ScanRange
 
 
 class LaserScanNode():
+
     def __init__(self):
         
         # create a publisher handle to publish ScanRange messages to the 
         # laserscan_range topic. Make sure you use self.var_name = xyz so
 	    # that you are able to use this handle in your other functions
        	#TODO: CREATE PUBLISHER HANDLER HERE
-        
+        self.scan_range = rospy.Publisher("laserscan_range", ScanRange, queue_size=10)
+
+        # Create an instance of the scan range message to store your closest and farthest
+        # points. Remember, these attributes can be accesed with:
+        # self.scan_range.closest_point. Also note the attributes are set to default values
+        # currently.
+       	#TODO: CREATE INSTANCE OF ScanRange HERE
+        self.scan = ScanRange()
+
         # subscriber handle for the scan message. This handle 
         # will subscribe to scan and recieve LaserScan messages. Each
         # time this happens the scan_callback function is called
@@ -23,11 +33,8 @@ class LaserScanNode():
         # initialize and register this node with the ROS master node
         rospy.init_node('laserscan_listen', anonymous=False)
         
-        # Create an instance of the scan range message to store your closest and farthest
-        # points. Remember, these attributes can be accesed with:
-        # self.scan_range.closest_point. Also note the attributes are set to default values
-        # currently.
-       	#TODO: CREATE INSTANCE OF ScanRange HERE
+
+
     
     # this is the callback for the scan message. 
     # here we will use the scan_data parameter to access the ranges 
@@ -38,6 +45,18 @@ class LaserScanNode():
         # in __init__()
         # TODO: FILTER DATA
         # TODO: FIND CLOSEST AND FARTHEST POINTS
+        if not self.scan.range_min:
+            self.scan.range_min = scan_data.ranges[0]
+            self.scan.range_max = scan_data.ranges[0]
+        self.scan.range_min = scan_data.ranges[0]
+        self.scan.range_max = scan_data.ranges[0]
+        for range in scan_data.ranges:
+            if range > self.scan.range_max and type(range) == float:
+                self.scan.range_max = range
+
+            if range < self.scan.range_min and type(range) == float:
+                self.scan.range_min = range
+
         pass
 
     # the publish method is  called on an interval in the main
@@ -45,17 +64,21 @@ class LaserScanNode():
     # the topic. 
     def publish(self):
        # TODO: ADD PUBLISHER CODE HERE
-        pass
+       self.scan_range.publish(self.scan)
+    pass
         
 
 if __name__ == '__main__':
     ls = LaserScanNode()
-
     rate = rospy.Rate(10) # 10hz
     try:
         # Add a while loop which calls the publish() function of the laser scan node
         # on the interval we have defined with rate
-        # TODO: CREATE PUBLISHER LOOP HERE 
+        # TODO: CREATE PUBLISHER LOOP HERE
+        while True: 
+            ls.publish()
+            rate.sleep()
+
         pass
     except rospy.ROSInterruptException:
         pass
