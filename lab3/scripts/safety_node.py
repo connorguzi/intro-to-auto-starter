846e2806c70573a40e57265d37e60f9c01d3b524
Connor Guzikowski <connorguzi@gmail.com>
2021-10-22 01:42:56 +0000

    Lab 3 almost done

--- lab3/scripts/safety_node.py
+++ lab3/scripts/safety_node.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+#from yaml import scan
 import rospy
 
 # TODO: import ROS msg types and libraries
+from ackermann_msgs.msg import AckermannDriveStamped
+from std_msgs.msg import Bool
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, Vector3
+from math import cos
 
 class Safety(object):
     """
@@ -18,16 +25,40 @@ class Safety(object):
         """
         self.speed = 0
         # TODO: create ROS subscribers and publishers.
+        self.ackermann = rospy.Publisher("brake", AckermannDriveStamped, queue_size=10)
+        self.bool = rospy.Publisher('brake_bool', Bool, queue_size=10)
+
+        self.bVal = Bool()
+        self.bVal.data = False
+        self.brake = AckermannDriveStamped()
+        
+        rospy.Subscriber('odom', Odometry, self.odom_callback)
+        rospy.Subscriber('scan', LaserScan, self.scan_callback)
         pass
 
     def odom_callback(self, odom_msg):
         # TODO: update current speed
-        self.speed = 0
+        self.speed = odom_msg.twist.twist.linear.x
 
     def scan_callback(self, scan_msg):
         # TODO: calculate TTC
-
+        TTC = []
+        theta = 0 - scan_msg.angle_min
+        for i in range(len(scan_msg.ranges)):
+            if type(scan_msg.ranges[i]) == float and (scan_msg.ranges[i]) != 0: 
+                theta = (0 - scan_msg.angle_min) + (scan_msg.angle_increment * i) 
+                TTC.append(scan_msg.ranges[i] / max([-self.speed * cos(theta), 0.00000000000001]))
         # TODO: publish brake message and publish controller bool
+        print(min(TTC))
+        if min(TTC) <= 1:
+            print('I am braking now')
+            self.bVal.data = True
+            self.bool.publish(self.bVal)
+            self.ackermann.publish(self.brake)
+        else:
+            self.bVal.data = False
+            self.bool.publish(self.bVal)
+            
         pass
 
 
