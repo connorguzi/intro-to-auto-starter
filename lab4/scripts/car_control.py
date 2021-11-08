78f776d4611d8d5f84304edb2f275e7a918dcb35
Connor Guzikowski <connorguzi@gmail.com>
2021-11-08 04:23:48 +0000

    Completed Lab4

--- lab4/scripts/car_control.py
+++ lab4/scripts/car_control.py
@@ -5,7 +5,7 @@ from lab4.msg import PIDInput
 from ackermann_msgs.msg import AckermannDriveStamped
 from geometry_msgs.msg import PoseStamped
 
-ERROR_SCALE = 0 # TODO: ASSIGN A CONSTANT FOR HOW MUCH TO SCALE THE RAW ERROR
+ERROR_SCALE = 5 # TODO: ASSIGN A CONSTANT FOR HOW MUCH TO SCALE THE RAW ERROR
 MAX_STEERING_ANGLE = 4.18
 
 class CarControl:
@@ -15,35 +15,40 @@ class CarControl:
 
         # get publisher handles for drive and pose topics 
         # TODO: CREATE A PUBLISHER FOR THE DRIVE TOPIC 
-
+        self.drive_pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
         # pose topic is to reset car position when you reset/shutdown the node
         self.pose_pub = rospy.Publisher('/pose', PoseStamped, queue_size=10)
-
+        self.past_error = 0
         # TODO: SUBSCRIBE TO dist_error MESSAGE FROM dist_finder.py NODE . ASSIGN THE CALLBACK TO run_pid()
-
+        rospy.Subscriber('dist_error', PIDInput, self.run_pid)
 
         # TODO: INITIALIZE KP AND KD CLASS VARIABLES FROM CONSTRUCTOR PARAMETERS
-        
+        self.kp = kP
+        self.kd = kD
         # assign shutdown functon 
         rospy.on_shutdown(self.on_shutdown)
 
     # callback for dist_error topic to handle pid calculation and publish to /drive. 
     def run_pid(self, pid_input):
         # TODO: SCALE ERROR BY SOME CONSTANT
-
+        error = pid_input.angle_error
+        error *= ERROR_SCALE
 
         # TODO: CALCULATE A STEERING ANGLE BASED BY PASSING THE ERROR INTO THE PID FUNCTION
- 
+        ut = self.kp * error + self.kd * (-self.past_error + error)
         # TODO: UPDATE PREVIOUS ERROR FOR DERIVATIVE TERM
-
+        self.past_error = error
 
         # TODO: VALIDATE STEERING ANGLE
-
-
+        steer = 3* ut/ MAX_STEERING_ANGLE
+        velocity = 14/abs((5*steer))
         # TODO: ASSIGN VALUES IN ACKERMANN MESSAGE 
-
+        ack_msg = AckermannDriveStamped()
+        ack_msg.drive.steering_angle = -steer
+        ack_msg.drive.speed = velocity
     
         # TODO PUBLISH ACKERMANN DRIVE MESSAGE
+        self.drive_pub.publish(ack_msg)
         # OPTIONAL: TO IMPROVE YOUR WALL FOLLOWING TRY ADDING ADAPTIVE VELOCITY CONTROL
         pass
 
@@ -59,8 +64,8 @@ class CarControl:
 
 if __name__ == "__main__":
     #  TODO: USE USER INPUT TO TUNE KP AND KD, THEN HARDCODE ONCE TUNED
-    kP = float(input("Enter kP: "))
-    kD = float(input("Enter kD: "))
+    kP = 0.42
+    kD = 0.11
 
     print("### START WALL FOLLOW ###")
 
--- lab4/scripts/dist_finder.py
+++ lab4/scripts/dist_finder.py
@@ -4,6 +4,7 @@ import rospy
 import math
 from sensor_msgs.msg import LaserScan
 from lab4.msg import PIDInput
+from nav_msgs.msg import Odometry
 
 # helpful constants:
 ANGLE_RANGE_DEG = 270
@@ -11,10 +12,10 @@ ANGLE_RANGE_RAD = 4.72
 CAR_LEN = 1.5
 
 
-DESIRED_DIST_FROM_WALL = 0 # TODO: SET A DESIRED DISTANCE AWAY FROM THE WALL
+DESIRED_DIST_FROM_WALL = 1.25 # TODO: SET A DESIRED DISTANCE AWAY FROM THE WALL
 
-WF_ZERO  = 0   # TODO: DEFINE WHAT YOUR "ZERO" ANGLE WILL BE TO FIND THE RANGE PERPENDICULAR TO THE CAR'S FORWARD VECTOR
-WF_THETA = 0   # TODO: SET THETA VALUE (HINT: SHOULD BE > WF_ZERO)
+WF_ZERO  = 45   # TODO: DEFINE WHAT YOUR "ZERO" ANGLE WILL BE TO FIND THE RANGE PERPENDICULAR TO THE CAR'S FORWARD VECTOR
+WF_THETA = 60   # TODO: SET THETA VALUE (HINT: SHOULD BE > WF_ZERO)
 
 
 class DistFinder:
@@ -29,15 +30,23 @@ class DistFinder:
         # TODO: CREATE AN PUBLISHER HANDLE TO PUBLISH TO THE /dist_error TOPIC
         self.error_pub = rospy.Publisher('/dist_error', PIDInput, queue_size=10)
 
+        # subscribe to the odom topic
+        rospy.Subscriber("/odom", Odometry, self.odom_callback)
         # subscribe to scan topic of simulator
         rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+
     
 
     # get_range converts a theta value in degrees to a range in the ranges array
     def get_range(self, data, theta):
         # TODO: USE THE MAPPING IN THE PRELAB OR A MAPPING OF YOUR CHOOSING TO 
         #       CONVERT FROM A THETA TO A RANGE IN THE DATA
-        return 0
+        dist = data.ranges[int(theta * (len(data.ranges)/270))]
+        return dist
+
+    def odom_callback(self, odom_msg):
+        self.speed = odom_msg.twist.twist.linear.x
+
 
     def scan_callback(self, data):
         
@@ -45,20 +54,26 @@ class DistFinder:
         # B IS GIVEN AS AN EXAMPLE, AND YOU NEED TO FILL IN THE REST
 
         b = self.get_range(data, WF_ZERO)	
-
+        a = self.get_range(data, WF_THETA)
 
         # math functions use radians, so use math.radians to convert for the alpha calculation
-        theta_rad = math.radians(WF_THETA)
+        theta_rad = math.radians(WF_THETA - WF_ZERO)
 
         ## Your code goes here to compute alpha, AB, and CD..and finally the error.
         # TODO: COMPUTE ALPHA, AB, CD AND THEN THE ERROR
+        AB = b * math.cos(math.atan((a * math.cos(theta_rad) - b)/(a * math.sin(theta_rad))))
+        AC = self.speed * 0.5
+        CD = AB + AC * math.atan((a * math.cos(theta_rad) - b)/(a * math.sin(theta_rad)))
+        #print(CD)
 
+        error =  CD - DESIRED_DIST_FROM_WALL
 
         # TODO: CREATE AN INSTANCE OF THE PIDInput() MESSAGE AND ASSIGN THE ANGLE ERROR
-    
+        inp = PIDInput()
+        inp.angle_error = error
 
         # TODO: PUBLISH YOUR MESSAGE USING self.error_pub
-        
+        self.error_pub.publish(inp)
 
 
 
